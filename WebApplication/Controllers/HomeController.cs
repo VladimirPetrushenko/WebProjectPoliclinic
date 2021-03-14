99f115036f059b12f4fb2621c80df9515c0c6a91
Vladimir Petrushenko <barrierofdust@gmail.com>
2021-03-14 21:19:58 +0300

    Add Web View MedicalCard
    Добавил отображение медицинской карточки на станицу Index.cshtml, сделал это при помощи модели mvc, логика доставания данных находится в контроллере

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project1;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            PoliclinicEDM context = new PoliclinicEDM();
+            var MedicalCard = context.MedicalCard;
+
+            List<MedicalCard> cards = new List<MedicalCard>();
+            foreach (var item in MedicalCard)
+            {
+                cards.Add(item);
+            }
+            return View(cards);
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
